eea3283a2c972f29b496e4a3dd39c30534be10ba
Luiz A C Darbem <luizdarbem@frwk.com.br>
2022-07-12 11:19:01 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return (<h1>Olá Mundo!</h1>)
+  return (
+    <>
+      <h1>Olá Mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+
+    </>
+  )
 }
 
 export default App
