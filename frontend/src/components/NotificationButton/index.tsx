ce65c83244c841aeceea324f2c1b95d3ae906482
Luiz A C Darbem <luizdarbem@frwk.com.br>
2022-07-17 19:07:35 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
 import './style.css';
@@ -10,7 +11,7 @@ type Props = {
 function handleClick(saleId: number) {
   axios(`${BASE_URL}/sales/${saleId}/notification`)
     .then(response => {
-      console.log("sucesso");
+      toast.info("SMS enviado com sucesso!");
     })
 }
 
