363c655e2004f2e0d7049b3280c48698a54074da
Luiz A C Darbem <luizdarbem@frwk.com.br>
2022-07-17 20:02:05 -0300

    criado o metodo post

--- backend/demo/src/main/java/com/br/reactSpring/demo/controllers/SaleController.java
+++ backend/demo/src/main/java/com/br/reactSpring/demo/controllers/SaleController.java
@@ -3,18 +3,25 @@ package com.br.reactSpring.demo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.reactSpring.demo.entities.Sale;
 import com.br.reactSpring.demo.services.SaleService;
 import com.br.reactSpring.demo.services.SmsService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping(value = "/sales")
+@Slf4j
 public class SaleController {
 	
 	@Autowired
@@ -34,6 +41,12 @@ public class SaleController {
 	@GetMapping("/{id}/notification")
 	public void notifySms(@PathVariable Long id) {
 		smsService.sendSms(id);
+	}
+	
+	@PostMapping
+	@ResponseStatus(code = HttpStatus.CREATED)
+	public void findSales(@RequestBody Sale sale){
+		log.info("Sale={}",sale);
 	}
 
 }
