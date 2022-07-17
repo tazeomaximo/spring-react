7016f744670542f32afb6ebc5bf4f03eef5efe92
Luiz A C Darbem <luizdarbem@frwk.com.br>
2022-07-17 13:05:23 -0300

    Twilio SMS

--- backend/demo/src/main/java/com/br/reactSpring/demo/controllers/SaleController.java
+++ backend/demo/src/main/java/com/br/reactSpring/demo/controllers/SaleController.java
@@ -4,12 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.br.reactSpring.demo.entities.Sale;
 import com.br.reactSpring.demo.services.SaleService;
+import com.br.reactSpring.demo.services.SmsService;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -18,12 +20,20 @@ public class SaleController {
 	@Autowired
 	private SaleService saleService;
 	
+	@Autowired
+	private SmsService smsService;
+	
 	@GetMapping
 	public Page<Sale> findSales(
 			@RequestParam(name = "minDate",defaultValue = "") String minDate, 
 			@RequestParam(name = "maxDate",defaultValue = "") String maxDate, 
 			Pageable pageable){
 		return saleService.findSales(minDate, maxDate, pageable);
+	}
+	
+	@GetMapping("/{id}/notification")
+	public void notifySms(@PathVariable Long id) {
+		smsService.sendSms(id);
 	}
 
 }
